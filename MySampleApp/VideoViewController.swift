ba81f55b335a01b41e04501b8c5086d260b60cbf
KeeSern <chuakeesern@gmail.com>
2016-11-09 17:50:49 -0500

    Conversation flow

--- MySampleApp/VideoViewController.swift
+++ MySampleApp/VideoViewController.swift
@@ -12,12 +12,20 @@ class VideoViewController: UIViewController,AVAudioPlayerDelegate {
         
     }
 
-    @IBAction func playVideo2(sender: UIButton) {
-        video1("https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+    @IBAction func sampleVideo(sender: UIButton) {
+        playSampleVideo("https://s3.amazonaws.com/aepios-userfiles-mobilehub-1099679197/public/SampleVideo.mp4")
     }
- 
+    @IBAction func playVideo1(sender: UIButton) {
+        video("https://s3.amazonaws.com/aepios-userfiles-mobilehub-1099679197/public/Lesson1Part1.mp4");
+    }
+    @IBAction func playVideo2(sender: UIButton) {
+    }
+    @IBAction func playVideo3(sender: UIButton) {
+    }
+    @IBAction func playVideo4(sender: UIButton) {
+    }
     
-    func video1(url:String){
+    func video(url:String){
         let videoURL = NSURL(string: url)
         let player = AVPlayer(URL: videoURL!)
         let playerViewController = AVPlayerViewController()
@@ -31,7 +39,27 @@ class VideoViewController: UIViewController,AVAudioPlayerDelegate {
                                                          object: playerViewController.player!.currentItem)
         
     }
+ 
+    func playSampleVideo(url:String){
+        let videoURL = NSURL(string: url)
+        let player = AVPlayer(URL: videoURL!)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.presentViewController(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sampleVideoPlayerDidFinishPlaying:",
+                                                         name: AVPlayerItemDidPlayToEndTimeNotification,
+                                                         object: playerViewController.player!.currentItem)
+        
+    }
     
+    func sampleVideoPlayerDidFinishPlaying(note:NSNotification){
+        print("finished")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
     
     func playerDidFinishPlaying(note:NSNotification){
         print("finished")
